5f1b5d6f2c902b7a9959da2546dee433b95d3aaf
Aaron M. Agosto <aaronagosto0613@gmail.com>
2023-07-20 15:39:02 -0400

    setting up deposit rules using while, if,else

--- main.py
+++ main.py
@@ -2,3 +2,11 @@
 def deposit():
     while True:
         amount = input("What Would you like to dposit? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else: 
+                print("Amount must be greater than 0.")
+        else:
+            print("Please return a number")          
